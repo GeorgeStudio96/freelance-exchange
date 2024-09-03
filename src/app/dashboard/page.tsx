119fc64909b41899d4880258d68b16d168aac145
George Ershov <64842499+GeorgeStudio96@users.noreply.github.com>
2024-09-03 16:37:37 +0300

    replace config

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-"use server";
-
 import Link from "next/link";
 import Counter from "@/utils/Counter";
 import { Suspense } from "react";
 import Loading from "./Loading";
-import { Metadata } from "next";
 
 export default async function HomePage() {
   // async function handleClick() {
